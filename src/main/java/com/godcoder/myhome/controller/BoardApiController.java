4a17135167551dc1aa0df874ce2b919704c1e1c4
Hojun <hojun9707@naver.com>
2022-02-28 20:10:03 +0900

    RESTful API

---
+++ src/main/java/com/godcoder/myhome/controller/BoardApiController.java
@@ -0,0 +1,58 @@
+package com.godcoder.myhome.controller;
+import java.util.List;
+
+import com.godcoder.myhome.model.Board;
+import com.godcoder.myhome.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.util.StringUtils;
+
+@RestController
+@RequestMapping("/api")
+class BoardApiController {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @GetMapping("/boards")
+    List<Board> all(@RequestParam(required = false, defaultValue = "") String title,
+                    @RequestParam(required = false, defaultValue = "") String content) {
+        if(StringUtils.isEmpty(title) && StringUtils.isEmpty(content)){
+            return boardRepository.findAll();
+        }else{
+            return boardRepository.findByTitleOrContent(title, content);
+        }
+    }
+
+    @PostMapping("/boards")
+    Board newBoard(@RequestBody Board newBoard) {
+        return boardRepository.save(newBoard);
+    }
+
+    // Single item
+
+    @GetMapping("/boards/{id}")
+    Board one(@PathVariable Long id) {
+        return boardRepository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/boards/{id}")
+    Board replaceBoard(@RequestBody Board newBoard, @PathVariable Long id) {
+
+        return boardRepository.findById(id)
+                .map(Board -> {
+                    Board.setTitle(newBoard.getTitle());
+                    Board.setContent(newBoard.getContent());
+                    return boardRepository.save(Board);
+                })
+                .orElseGet(() -> {
+                    newBoard.setId(id);
+                    return boardRepository.save(newBoard);
+                });
+    }
+
+    @DeleteMapping("/boards/{id}")
+    void deleteBoard(@PathVariable Long id) {
+        boardRepository.deleteById(id);
+    }
+}
